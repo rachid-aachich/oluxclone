f6ee5a735e38713438f50f8c604b798df60e93fe
Mohamed Amine <mam@terabyte-software.com>
2019-10-31 02:44:38 +0100

    Reports & Tickets

--- application/controllers/Support.php
+++ application/controllers/Support.php
@@ -6,20 +6,53 @@ Class Support extends CI_Controller {
   public function __construct()
   {
     parent::__construct();
-    
-    $this->template->displayHeader();
+    $this->load->model('ticket');
+    $this->load->model('report');
   }
   
   public function tickets()
   {
+    $this->template->displayHeader();
     $this->template->display('support/tickets');
     $this->template->displayFooter();
   }
   
   public function reports()
   {
+    $this->template->displayHeader();
     $this->template->display('support/reports');
     $this->template->displayFooter();
+  }
+
+  public function getTickets(){
+    $response_data = $this->ticket->getList();
+    header('Content-type: application/json');
+    echo json_encode(["response_type"=>"success","response_data"=>$response_data]);
+    return; 
+  }
+
+  public function getReports(){
+    $response_data = $this->report->getList();
+    header('Content-type: application/json');
+    echo json_encode(["response_type"=>"success","response_data"=>$response_data]);
+    return; 
+  }
+
+
+  public function save(){
+    if(!$_POST["operation"]) {
+      header("HTTP/1.1 401 Unauthorized");
+      exit;
+    }
+
+    $response = "";
+    if($_POST["operation"] == "add_ticket") $response = $this->ticket->add($_POST["title"],$_POST["message"]);
+    else if($_POST["operation"] == "add_report") $this->report->add($_POST["title"],$_POST["message"]);
+       
+    header('Content-type: application/json');
+    echo json_encode(["response_type"=>"success"]);
+
+    return; 
   }
   
 }
--- application/models/Report.php
+++ application/models/Report.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 Class Report extends CI_Model {
   
   private $id;
-  private $table_name = 'cpanel';
+  private $table_name = 'reports';
   
   public function __construct()
   {
@@ -20,6 +20,20 @@ Class Report extends CI_Model {
   public function getId()
   {
     return $this->id;
+  }
+
+  public function getList()
+  {
+    $user_id = 1;
+    $this->db->select('reports.*,messages.message,user.name seller, orders.item_type');
+    $this->db->from($this->table_name);
+    $this->db->join('messages', 'messages.id = reports.last_reply','left');
+    $this->db->join('orders', 'orders.id = reports.order_id','left');
+    $this->db->join('user', 'user.id = orders.seller_id','left');
+    
+    $this->db->where('reports.user_id',$user_id);
+    $query = $this->db->get();
+    return $query->result();
   }
   
   public function add()
@@ -41,12 +55,5 @@ Class Report extends CI_Model {
   {
     
   }
-
-
-	public function getList()
-	{
-		$query = $this->db->get($this->table_name);
-		return $query->result();
-	}
 
 }
